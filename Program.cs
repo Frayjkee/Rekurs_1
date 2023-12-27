c35dddf75d3b24cee5a6df59770378095c490316
Ilya Maksimov <iluxamax@yandex.ru>
2023-12-27 15:25:14 +0300

    Рабочая версия программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение M:");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение N:");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNumbersInRange(m, n);
+
+        Console.ReadLine(); // Чтобы консольное окно не закрывалось сразу
+    }
+
+    static void PrintNumbersInRange(int start, int end)
+    {
+        if (start <= end)
+        {
+            Console.WriteLine(start);
+            PrintNumbersInRange(start + 1, end);
+        }
+    }
+}
